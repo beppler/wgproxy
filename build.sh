ad78526b81ed4c26ec61d5ad0d2a925a6439761c
Carlos Alberto Costa Beppler <beppler@gmail.com>
2026-09-25 16:44:30 -0300

    FIx build script to generate the correct binary.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+trap 'echo "An error has occurred! Aborting the script execution..." >&2' ERR
+
 mkdir -p dist/
 
 rm -f dist/*
@@ -7,6 +10,8 @@ rm -f dist/*
 package=`grep module go.mod | cut -d " " -f 2`
 package_split=(${package//\// })
 package_name=${package_split[-1]}
+
+main_package=$package/cmd/$package_name
 
 platforms=("windows/amd64" "windows/386" "windows/arm64" "linux/amd64" "linux/386" "linux/arm64" "darwin/amd64" "darwin/arm64")
 
@@ -24,12 +29,8 @@ do
         archive_name+='.tar.gz'
     fi
 
-    echo "CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o ""dist/$output_name"" $package"
-    CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "dist/$output_name" $package
-    if [ $? -ne 0 ]; then
-        echo 'An error has occurred! Aborting the script execution...'
-        exit 1
-    fi
+    echo "CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o ""dist/$output_name"" $main_package"
+    CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "dist/$output_name" $main_package
     echo "go run tools/archive.go ""dist/$archive_name"" ""dist/$output_name"" README.md LICENSE"
     go run tools/archive.go "dist/$archive_name" "dist/$output_name" README.md LICENSE
     rm "dist/$output_name"
